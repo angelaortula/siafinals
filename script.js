9cdbabdf7d8b49ed21ca75818a7269f4e00cba7f
Angela Ortula <angelaortula28@gmail.com>
2025-05-18 13:46:02 +0800

    boracay page

---
+++ script.js
@@ -0,0 +1,5 @@
+// Placeholder for any future interactive features
+console.log("Philippines tourism website loaded.");
+function learnMore(place) {
+  alert("More information about " + place + " coming soon!");
+}
